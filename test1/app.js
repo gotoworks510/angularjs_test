2940d9943e957fbfd51520a78523b3edd88bdad2
Yoshitaka Senna Goto <yoshi@opdsgn.com>
2014-11-30 07:42:31 -0800

    Initial Commit

---
+++ test1/app.js
@@ -0,0 +1,9 @@
+function MainCtrl($scope) {
+  $scope.people = [
+    { firstName: 'Kazuhito', lastName: 'Hokamura', url: 'http://webya.opdsgn.com/' },
+    { firstName: 'Takeshi',  lastName: 'Takatsudo', url: 'http://webya.opdsgn.com/' },
+    { firstName: 'Akihiro',  lastName: 'Oyamada', url: 'http://webya.opdsgn.com/' },
+    { firstName: 'Kazuhori',  lastName: 'Tokuda', url: 'http://webya.opdsgn.com/' },
+    { firstName: 'Yukihisa',  lastName: 'Yamada', url: 'http://webya.opdsgn.com/' },
+  ];
+}
